4ac6f63d79a597d0598fc07cdddbc7447bbcf461
Gekif <dzulfikar.maulana@gmail.com>
2020-03-22 20:53:11 +0700

    011 Writing the JavaScript Code Part 3

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,19 +1,39 @@
-$(document).ready(function () {
-
+$(document).ready(function(){
     $.ajaxSetup({
-        header: {
+        headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
 
-    $('.follow-button').click(function () {
+    $('.follow-button').click(function(){
         let user_id = $(this).data('id');
         let object = $(this);
-        let currentFollowerCount = parseInt($(this).parent("div").find(".follower").text());
+        var currentFollowerCount = parseInt($(this).parent("div").find(".follower").text());
 
-        console.log(user_id);
-        console.log(currentFollowerCount);
+        $.ajax({
+            type: 'POST',
 
+            url: '/ajax',
+
+            data: {user_id: user_id},
+
+            success: function(data){
+                console.log(data.success);
+
+                if ($.isEmptyObject(data.success.attached)){
+                    object.find("strong").text("Follow");
+
+                    object.parent("div").find(".follower").text(currentFollowerCount - 1);
+
+                } else {
+                    object.find("strong").text("UnFollow");
+
+                    object.parent("div").find(".follower").text(currentFollowerCount + 1);
+                }
+
+            }
+
+        });
 
     });
 
