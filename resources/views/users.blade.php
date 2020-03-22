597b7c0d5ab324a58e0c046f25acb9af51df8348
Gekif <dzulfikar.maulana@gmail.com>
2020-03-22 20:32:27 +0700

    007 Adding some Methods to the Users List and Using It

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,15 +1,30 @@
-@if ($users->count())
-
-    @foreach ($users as $user)
+@if($users->count())
+    @foreach($users as $user)
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title">Name of Person</h5>
-                <small>Following <span class="badge badge-primary">1</span></small>
-                <small>Follower <span class="badge badge-primary follower">20</span></small>
+                <h5 class="card-title"><a href="{{route('user', $user->id)}}">{{$user->name}}</a></h5>
+                <img class="card-img-top img-responsive" src="{{asset('images')}}/person{{$user->id}}.jpeg" alt="">
+                <small>Following
+                    <span
+                            class="badge badge-primary">{{$user->followings()->get()->count()}}</span></small>
+                <small>Follower
+                    <span
+                            class="badge badge-primary follower">{{$user->followers()->get()->count()}}</span></small>
             </div>
-
-            <button>Follow</button>
+            <button style="margin: 10px" class="btn btn-primary follow-button" data-id="{{$user->id}}">
+                <strong>
+                    @if(auth()->user()->isFollowing($user))
+                        UnFollow
+                    @else
+                        Follow
+                    @endif
+                </strong>
+            </button>
         </div>
     @endforeach
+
+@else
+
+    <h1>Nothing Yet!</h1>
 
 @endif
