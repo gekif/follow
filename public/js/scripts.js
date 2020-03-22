6ec25dbf2f32f38cf71acb696912db4118fa1c51
Gekif <dzulfikar.maulana@gmail.com>
2020-03-22 20:42:12 +0700

    010 Writing the JavaScript Code Part 2

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,20 @@
 $(document).ready(function () {
 
     $.ajaxSetup({
-
         header: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
+    });
+
+    $('.follow-button').click(function () {
+        let user_id = $(this).data('id');
+        let object = $(this);
+        let currentFollowerCount = parseInt($(this).parent("div").find(".follower").text());
+
+        console.log(user_id);
+        console.log(currentFollowerCount);
+
+
     });
 
 });
